c8384db8a903a5c94e0ca42a46dfa491bb780a28
Honorata Łyczak <honoratalyczak@MacBook-Pro-Honorata.local>
2023-11-13 22:14:52 +0100

    add post method

--- src/components/routes/AddPhotos.tsx
+++ src/components/routes/AddPhotos.tsx
@@ -1,15 +1,33 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 export default function AddPhotos() {
   return (
     <div>
-      <TextField
-        error
-        id="outlined-error-helper-text"
-        label="Error"
-        defaultValue="Hello World"
-        helperText="Incorrect entry."
-      />
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts",
+            {
+              method: "POST",
+              body: formData,
+            }ś
+          );
+          const data = await response.json();
+          console.log(data);
+        }}
+      >
+        <TextField
+          id="userId"
+          name="userId"
+          label="UserId"
+          defaultValue="/userId"
+        />
+        <TextField id="title" name="title" label="Title" defaultValue="Title" />
+        <TextField id="body" name="body" label="Body" defaultValue="Body" />
+        <Button type="submit">Submmit</Button>
+      </form>
     </div>
   );
 }
